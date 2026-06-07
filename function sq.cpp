0cde6a338ba94dd3a4b1d144bbe762a413ff8f36
ashimarawat78 <ashimarawat78@gmail.com>
2026-06-07 11:28:36 +0530

    sq of no using function

---
+++ function sq.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+  int data ( int a) {
+       return a*a;
+      }
+  int main(){
+    int a;
+  cout<<" enter the value of a:";
+   cin>>a;
+  cout<<data(a);
+        
+    return 0;
+}
