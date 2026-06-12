951ec7e1148cf4e5f299ada6959517e07ba5b759
ashimarawat78 <ashimarawat78@gmail.com>
2026-06-12 14:40:36 +0530

    sum of digit

---
+++ sum_of_digit.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int n,digit ,sum=0;
+    cout<<"enter the value:";
+    cin>> n;
+ while(n>0){
+     digit =n%10;
+     sum= sum+digit;
+     n=n/10;
+ }
+cout<<" sum is :"<<sum <<" ";
+return 0;
+}
