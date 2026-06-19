a0a7b64101ac51bf21026ef4258870c695a226af
ashimarawat78 <ashimarawat78@gmail.com>
2026-06-19 20:25:38 +0530

    STUDENT_DETAILS

---
+++ STUDENT_DETAILS.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main (){
+    string name;
+    int rollno;
+    float marks;
+    cout<<" enter the name :";
+    cin>> name;
+    cout<<" enter the roll no :"<< endl;
+    cin>> rollno;
+    cout<<" enter the marks :"<< endl;
+    cin>> marks;
+    cout<<" ------STUDENT DETAILS-------"<< endl<<endl<<endl;
+    cout<<"Name:"<<name<< endl;
+     cout<<"Rollno:"<<rollno<< endl;
+      cout<<"Marks:"<<marks<< endl;
+    return 0;
+}
