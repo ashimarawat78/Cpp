691bcea29d771f0f07986d55209db82ee10c2d2b
ashimarawat78 <ashimarawat78@gmail.com>
2026-06-24 16:29:29 +0530

    Ceiling_of_a_Number

---
+++ Ceiling_of_a_Number.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int BinarySearch(int arr[], int size ,int key){
+    int start=0;
+    int end=size-1;
+    int ans=-1;
+    int mid= start+(end-start)/2;
+    while(start<=end){
+        if(arr[mid]== key){
+            return mid;
+        }else if(arr[mid]> key){
+            ans= arr[mid];
+            end= mid-1;
+        }else{
+            start= mid+1;
+        }
+      mid= start+(end-start)/2;
+    }
+      return ans;
+    }
+    int main(){
+        int arr[5]={2, 4, 6, 8, 10};
+        int element= BinarySearch(arr,5,5);
+cout<<" the element is "<<element <<endl;
+return 0;
+    }
