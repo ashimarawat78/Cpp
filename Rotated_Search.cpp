21e4ac0824c3a5901699ed15a90c17ccf7915796
ashimarawat78 <ashimarawat78@gmail.com>
2026-06-24 15:45:13 +0530

    Rotated_Search

---
+++ Rotated_Search.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int Pivot(int arr[],int size){
+    int start= 0;
+    int end= size-1;
+    int mid=start+(end-start)/2;
+    while(start<end){
+        if(arr[mid]>arr[0]){
+            start= mid+1;
+        }else{
+            end=mid;
+        }
+         mid=start+(end-start)/2;
+    }
+    return start;
+}
+int BinarySearch(int arr[],int start, int end, int key){
+int mid=start+(end-start)/2;
+while(start<=end){
+    if(arr[mid]==key){
+        return arr[mid];
+        }
+        else if(arr[mid]<key){
+            start= mid+1;
+        }else{
+            end= mid-1;
+        }
+        mid=start+(end-start)/2;
+}
+return -1;
+}
+int Rotated(int arr[], int size, int key){
+    int pivot= Pivot(arr, size);
+    if(arr[pivot]<=key&&key<=size-1){
+        return BinarySearch(arr,pivot,size-1, key);
+    }else{
+        return BinarySearch(arr,0,pivot-1,key);
+    }
+}
+int main(){
+ int arr[5] = {7, 9, 1, 2, 3};
+ int index=Rotated(arr,5,2);
+ cout<<" The index is:"<<index;
+ return 0;
+}
