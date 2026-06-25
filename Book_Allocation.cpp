f476d08a8ed2aba26a67fb88638f7e12d6b01487
ashimarawat78 <ashimarawat78@gmail.com>
2026-06-25 16:18:57 +0530

    Book_Allocation

---
+++ Book_Allocation.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+bool Possible(int arr[], int n, int m, int mid){
+    int studentCount=1;
+    int pageSum= 0;
+    for(int i=0;i<n;i++){
+        if(pageSum+arr[i]<=mid){
+            pageSum+=arr[i];
+        }else{
+            studentCount++;
+            if(studentCount>m || arr[i]>mid){
+                return false;
+            }
+            pageSum = arr[i];
+        }
+    }
+    return true;
+}
+int Binary(int arr[], int n, int m){
+    int start= 0;
+    int sum =0;
+    for(int i=0; i<n; i++){
+        sum+=arr[i];
+    }
+    int end= sum;
+    int ans=-1;
+    int mid= start+(end-start)/2;
+    while(start<=end){
+        if(Possible(arr, n ,m ,mid)){
+            ans= mid;
+            end = mid-1;
+        }else{
+            start= mid+1;
+        }
+        mid= start+(end-start)/2;
+    }
+    return ans;
+}
+int main(){
+    int arr[4]={10,20,30,40};
+    int n=4;
+    int m=2;
+    int Minimum= Binary(arr, 4, 2);
+    cout<<" the Minimum is:"<< Minimum<<endl;
+    return 0;
+}
